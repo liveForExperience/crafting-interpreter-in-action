db88c8a746cdf5882f037fc5841d32a367882f31
Chen Yue <liveforexperience@126.com>
2025-08-04 23:30:47 +0800

    feat: function

--- src/main/java/cn/lfe/lox/Stmt.java
+++ src/main/java/cn/lfe/lox/Stmt.java
@@ -4,9 +4,11 @@ import java.util.List;
 abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
-    R visitIfStmt(If stmt);
     R visitExpressionStmt(Expression stmt);
+    R visitFunctionStmt(Function stmt);
+    R visitIfStmt(If stmt);
     R visitPrintStmt(Print stmt);
+    R visitReturnStmt(Return stmt);
     R visitVarStmt(Var stmt);
     R visitWhileStmt(While stmt);
   }
@@ -22,6 +24,36 @@ abstract class Stmt {
     }
 
     final List<Stmt> statements;
+  }
+
+  static class Expression extends Stmt {
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+
+    final Expr expression;
+  }
+
+  static class Function extends Stmt {
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+    }
+
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
   }
 
   static class If extends Stmt {
@@ -41,19 +73,6 @@ abstract class Stmt {
     final Stmt elseBranch;
   }
 
-  static class Expression extends Stmt {
-    Expression(Expr expression) {
-      this.expression = expression;
-    }
-
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-      return visitor.visitExpressionStmt(this);
-    }
-
-    final Expr expression;
-  }
-
   static class Print extends Stmt {
     Print(Expr expression) {
       this.expression = expression;
@@ -65,6 +84,21 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+
+  static class Return extends Stmt {
+    Return(Token keyword, Expr value) {
+      this.keyword = keyword;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitReturnStmt(this);
+    }
+
+    final Token keyword;
+    final Expr value;
   }
 
   static class Var extends Stmt {
